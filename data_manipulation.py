218763f79b7739c2c87a873247813e4c2ac77bea
IchRak-1999 <114731814+IchRak-1999@users.noreply.github.com>
2023-12-15 11:09:08 +0100

    Ajouter un map et un histogramme

--- data_manipulation.py
+++ data_manipulation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px
 
 def readData(fileName):
     data = pd.read_excel(fileName)
@@ -7,5 +8,27 @@ def readData(fileName):
     print(data["Région"])
     """
 
-def mapDepitRegion():
-    data = readData("nombre_debit_departement.xlsx")
+def getPourcentageDebitFigure(data):
+    map = px.line(data,x=data['Région'],y=data['Nombre de locaux'], title='Pourcentage de débit par région')
+    return map
+
+def displayMap():
+     data = readData('Pourcentage_debit_national.xlsx')
+
+     # Couleur carte : blanc -> jaune -> rouge
+     #colorscale = ["rgb(255,255,255)", "rgb(255,226,58)", "rgb(245,164,12)", "rgb(255,150,150"]
+"""
+     map = px.choropleth_mapbox(data,
+         center = {"lon": -2.573158317817201, "lat": 46.75212876753963},
+         color_continuous_scale = colorscale,
+         color_continuous_midpoint = 0
+     )"""
+
+
+def histDebitNational():
+    data = readData('Pourcentage_debit_national.xlsx')
+    mbit = ['0,5 Mbit/s','3 Mbit/s','8 Mbit/s','30 Mbit/s']
+    debit = data[mbit]
+
+    fig = px.histogram(debit)
+    return  fig
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from data_manipulation import readData
+import folium as folium
+
+from data_manipulation import readData,getPourcentageDebitFigure,displayMap,histDebitNational
 from dash import html
 import dash
 import pandas as pd
@@ -7,28 +9,37 @@ from dash import Dash, dcc
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-data = pd.read_excel("nombre_debit_departement.xlsx")
-
-
-fileName = "nombre_debit_departement.xlsx"
+fileName = "Pourcentage_debit_departement.xlsx"
 data = readData(fileName)
-# print(data)
+nomRegion = data["Région"]
 
 app = dash.Dash(__name__)
 
 app.title = "Débit"
+
+# Créer une carte Folium
+mymap = folium.Map(location=[48.8566, 2.3522], zoom_start=12)
 
 # Interface de la dashboard
 app.layout = html.Div([
     # Titre
     html.H1("DEBIT", style={'color': '#7E7288', 'textAlign': 'center'}),
 
+    # Ajouter le map
+    html.Iframe(id='map', srcDoc=mymap._repr_html_(), width='100%', height='450'),
+
     dcc.Graph(
         id="map",
-    )
+        figure = getPourcentageDebitFigure(data)
+    ),
+    dcc.Graph(
+        figure=displayMap()
+    ),
+    dcc.Graph(
+        figure=histDebitNational()
+    ),
 
 ], style={'overflowX': 'hidden'})
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
