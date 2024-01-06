73065d71c27784ef803d4a09a8c0f8d776db8151
IchRak-1999 <114731814+IchRak-1999@users.noreply.github.com>
2024-01-06 15:29:11 +0100

    Version final de code

--- data_manipulation.py
+++ data_manipulation.py
@@ -3,12 +3,27 @@ import plotly.express as px
 
 
 def readData(fileName):
+    """
+        Lire les donées depuis un fichier Excel
+
+           Args:
+               - fileNme: contient le nom de fichier
+
+           Returns:
+               Les données de le fichier fileName
+    """
     # Lire les données de fichier Excel
     data = pd.read_excel(fileName)
     return data
 
 
 def histDebitNational():
+    """
+       Affichage de la graphe funnel de débit national
+
+           Returns:
+               Affiche la graphe funnel (fig)
+           """
     # Afficher le pourcentage de débit national
     data = dict(
         vitesse=['','0,5 Mbit/s', '3 Mbit/s', '8 Mbit/s', '30 Mbit/s'],
@@ -19,7 +34,13 @@ def histDebitNational():
 
 
 def graph_hist():
-    # Afficher le pourcentage de débit par département
+    """
+        Affichage de la graphe de nombre des locaux national
+
+           Returns:
+               Affiche l'histogramme (fig)
+           """
+    # Afficher le nombre des locaux national
     data = readData("Pourcentage_debit_departement.xlsx")
-    fig = px.histogram(data, x='Région', y='Nombre de locaux', title='Pourcentage Débit par département')
+    fig = px.histogram(data, x='Région', y='Nombre de locaux', title='Nombre des locaux par département')
     return fig
